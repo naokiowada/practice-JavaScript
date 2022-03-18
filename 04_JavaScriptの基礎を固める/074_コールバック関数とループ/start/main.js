0333f882b2cdc880d24aadb9d19c1c593070e15c
naokiowada <naoki.owada.709@gmail.com>
2022-03-19 02:26:57 +0900

    [Add] コールバック関数とループ処理について

---
+++ 04_JavaScriptの基礎を固める/074_コールバック関数とループ/start/main.js
@@ -0,0 +1,20 @@
+const array = [1,2,3,4,5];
+
+function forEach(ary, callback) {
+    for(let i = 0; i < ary.length; i++) {
+        callback(ary[i]);
+    }
+}
+
+function log(val) {
+    console.log(val);
+}
+
+function doble(val) {
+    val *= 2;
+    log(val);
+}
+
+forEach(array, doble);
+
+
